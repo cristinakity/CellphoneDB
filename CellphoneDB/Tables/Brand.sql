c98c155985422ed47f9b8f7825306d5ad0be65de
Cristina Carrasco <cristinakity@hotmail.com>
2023-05-22 20:59:53 -0500

    Add project files.

---
+++ CellphoneDB/Tables/Brand.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Brands]
+(
+	[BrandId] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	[Name] NVARCHAR(50) NOT NULL
+)
---
+++ CellphoneDB/Tables/Cellphones.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Cellphones]
+(
+    [CellphoneId] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+    [Name] NVARCHAR(50) NOT NULL,
+    [Price] DECIMAL(18,2) NOT NULL,
+    [Description] NVARCHAR(100) NOT NULL,
+    [Image] NVARCHAR(100) NOT NULL,
+    [BrandId] INT NOT NULL,
+    [LaunchDate] DATETIME NOT NULL,
+    CONSTRAINT [FK_Cellphones_Brands] FOREIGN KEY ([BrandId]) REFERENCES [dbo].[Brands] ([BrandId])
+)
